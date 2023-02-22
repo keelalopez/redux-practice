5a9b52d17d5221002aedeea2379a249cc46e5e57
Keila Lopez <keilalofra@gmail.com>
2023-02-22 11:14:26 -0800

    Starting to set up store

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// STEP 1: IMPORT CREATESTORE METHOD AND SAVE IN A VARIABLE. 
+import { createStore } from "redux";
 
 ReactDOM.render(
   <App />,
